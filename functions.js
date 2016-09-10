e7dde8e9273f1e56a9ab7b7a1674fa0b1d8a7566
tusharbose92 <bosetushar1992@gmail.com>
2016-09-10 16:59:40 +0530

    First Commit

---
+++ functions.js
@@ -0,0 +1,31 @@
+//------Variables and Functions------//
+/*
+      function updateOutput(){
+          $("iframe").contents().find("html").html("<html><head><style type='text/css'>" + $("#cssPanel").val() + "</style></head><body>" + $("#htmlPanel").val() + "</body></html>");
+
+          document.getElementById('outputPanel').contentWindow.eval($("#javascriptPanel").val());
+
+      }
+
+
+      //-------text box content changes-------//
+
+      updateOutput();
+
+      $("textarea").on('change keyup paste', function(){
+
+      updateOutput();
+
+      });
+
+      //-------Button-toggle-view---------//
+
+      $(".toggleButton").click(function(){
+        var panelId = $(this).attr("id") + "Panel";
+        $("#" + panelId).toggle();
+      })
+
+      $(".toggleButton").click(function(){
+        $(this).toggleClass("active");
+      })
+*/
